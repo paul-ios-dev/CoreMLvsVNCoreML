9008703c53d4c6576cfe76ff373c8a82e4c8b36b
paulchainkang <paul@paul-kang.com>
2018-10-06 17:30:00 +0800

    Initial Commit

---
+++ CoreMLvsVNCoreML/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreMLvsVNCoreML
+//
+//  Created by Kang Paul on 2018/10/6.
+//  Copyright © 2018年 Kang Paul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
